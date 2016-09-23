1a9d998dcfaa149b29625ee828e749e1bbb9b1a7
Peregrin <peregringaret@gmail.com>
2016-09-23 02:02:26 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,53 @@
 # Write your code here.
+$dictionary = {
+  "hello"=>"hi",
+  "to"=>"2",
+  "two"=>"2",
+  "too"=>"2",
+  "for"=>"4",
+  "four"=>"4",
+  "be"=>"b",
+  "you"=>"u",
+  "at"=>"@",
+  "and"=>"&"
+}
+
+def word_substituter(tweet)
+  tweet = tweet.split(' ')
+  tweet = tweet.collect do |x|
+    if $dictionary.keys.include?(x) || $dictionary.keys.include?(x.downcase)
+      x = $dictionary[x.downcase]
+    end
+    x
+  end
+  tweet.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |x|
+    puts word_substituter(x)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length < 140
+    tweet
+  else
+    tweet = word_substituter(tweet)
+    if tweet.length < 140
+      tweet
+    else
+      tweet = tweet.slice(0, 137)
+      tweet = tweet+"..."
+      tweet
+    end
+  end
+end
